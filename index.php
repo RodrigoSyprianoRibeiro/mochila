d648998460b65e9a53982306c46752fed2148956
Rodrigo Ribeiro <rodrigo.sypriano.riebrio@gmail.com>
2016-03-30 09:17:46 -0300

    Adicionado código fonte e conteúdo sobre o problema da mochila.

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
     <link href="css/responsive.css" rel="stylesheet">
     <link href="css/ionslider/ion.rangeSlider.css" rel="stylesheet">
     <link href="css/ionslider/ion.rangeSlider.skinNice.css" rel="stylesheet">
+    <link href="css/rainbow/blackboard.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
     <!--[if lt IE 9]>
     <script src="js/html5shiv.js"></script>
@@ -60,7 +61,9 @@
                 <div class="col-md-12">
                     <ul id="tab1" class="nav nav-tabs">
                         <li class="active"><a href="#tab1-item1" data-toggle="tab">Mochila</a></li>
-                        <li><a href="#tab1-item2" data-toggle="tab">Log</a></li>
+                        <li><a href="#tab1-item2" data-toggle="tab">Código</a></li>
+                        <li><a href="#tab1-item3" data-toggle="tab">Sobre</a></li>
+                        <li><a href="#tab1-item4" data-toggle="tab">Log</a></li>
                     </ul>
                     <div class="tab-content">
                         <div class="tab-pane fade active in" id="tab1-item1">
@@ -107,6 +110,88 @@
                         </div>
                         <div class="tab-pane fade" id="tab1-item2">
                             <div class="col-sm-12 wow fadeIn" data-wow-duration="500ms" data-wow-delay="300ms">
+                                <h2 class="page-header">Código <strong>Fonte</strong></h2>
+                                <a href="https://github.com/RodrigoSyprianoRibeiro/mochila" target="_blank"><i class="fa fa-github"></i> GitHub</a>
+                                <div class="col-md-12">
+                                    <ul id="tab2" class="nav nav-tabs">
+                                        <li class="active"><a href="#tab2-item1" data-toggle="tab">HTML</a></li>
+                                        <li><a href="#tab2-item2" data-toggle="tab">CSS</a></li>
+                                        <li><a href="#tab2-item3" data-toggle="tab">JS</a></li>
+                                        <li><a href="#tab2-item4" data-toggle="tab">executar.php</a></li>
+                                        <li><a href="#tab2-item5" data-toggle="tab">AlgoritmosGeneticos.php</a></li>
+                                        <li><a href="#tab2-item6" data-toggle="tab">Cromossomo.php</a></li>
+                                        <li><a href="#tab2-item7" data-toggle="tab">Artigo.php</a></li>
+                                        <li><a href="#tab2-item8" data-toggle="tab">Util.php</a></li>
+                                    </ul>
+                                    <div class="tab-content">
+                                        <div class="tab-pane fade active in" id="tab2-item1">
+                                            <pre><code data-language="html"><?php include 'docs/html.txt'; ?><</code></pre>
+                                        </div>
+                                        <div class="tab-pane fade" id="tab2-item2">
+                                            <pre><code data-language="css"><?php include 'docs/css.txt'; ?></code></pre>
+                                        </div>
+                                        <div class="tab-pane fade" id="tab2-item3">
+                                            <pre><code data-language="javascript"><?php include 'docs/js.txt'; ?></code></pre>
+                                        </div>
+                                        <div class="tab-pane fade" id="tab2-item4">
+                                            <pre><code data-language="php"><?php include 'docs/executar.txt'; ?></code></pre>
+                                        </div>
+                                        <div class="tab-pane fade" id="tab2-item5">
+                                            <pre><code data-language="php"><?php include 'docs/algoritmosgeneticos.txt'; ?></code></pre>
+                                        </div>
+                                        <div class="tab-pane fade" id="tab2-item6">
+                                            <pre><code data-language="php"><?php include 'docs/cromossomo.txt'; ?></code></pre>
+                                        </div>
+                                        <div class="tab-pane fade" id="tab2-item7">
+                                            <pre><code data-language="php"><?php include 'docs/artigo.txt'; ?></code></pre>
+                                        </div>
+                                        <div class="tab-pane fade" id="tab2-item8">
+                                            <pre><code data-language="php"><?php include 'docs/util.txt'; ?></code></pre>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tab-pane fade" id="tab1-item3">
+                            <div class="col-sm-12 wow fadeIn" data-wow-duration="500ms" data-wow-delay="300ms">
+                                <h2 class="page-header">Sobre o Problema da <strong>Mochila</strong></h2>
+                                <blockquote>
+                                    <p>O problema da mochila é um problema de optimização combinatória.
+                                    O nome dá-se devido ao modelo de uma situação em que é necessário preencher
+                                    uma mochila com objetos de diferentes pesos e valores. O objetivo é que se
+                                    preencha a mochila com o maior valor possível, não ultrapassando o peso máximo.</p>
+                                </blockquote>
+                            </div>
+                            <div class="col-sm-12 wow fadeIn" data-wow-duration="500ms" data-wow-delay="300ms">
+                                <h2 class="page-header">Algoritmos <strong>Genéticos</strong></h2>
+                                <blockquote>
+                                    <p>Para resolver o problema foi utilizada as técnicas dos <b>Algoritmos Genéticos</b>.
+                                    São algoritmos de busca baseados nos mecanismos de seleção natural e genética.</p>
+
+                                    <p>Componentes dos <b>Algoritmos Genéticos:</b>
+                                    <ul>
+                                        <li>Representação (definição dos indivíduos)</li>
+                                        <li>Função de avaliação (função fitness)</li>
+                                        <li>População</li>
+                                        <li>Mecanismo de seleção dos pais</li>
+                                        <li>Operadores genéticos</li>
+                                        <li>Mecanismo de seleção dos sobreviventes</li>
+                                    </ul>
+                                    </p>
+                                </blockquote>
+                            </div>
+                            <div class="col-sm-12 wow fadeIn" data-wow-duration="500ms" data-wow-delay="300ms">
+                                <h2 class="page-header"><strong>Autores</strong></h2>
+                                <blockquote>
+                                    <p>Rodrigo Ribeiro e Taynara Rechia.</p>
+
+                                    <footer>Trabalho da disciplina <cite title="Modelos Evolucionários e Tratamento de Incertezas">Modelos Evolucionários e Tratamento de Incertezas</cite>
+                                    do curso de Ciência da Computação da UNISUL (Universidade do Sul de Santa Catarina).</footer>
+                                </blockquote>
+                            </div>
+                        </div>
+                        <div class="tab-pane fade" id="tab1-item4">
+                            <div class="col-sm-12 wow fadeIn" data-wow-duration="500ms" data-wow-delay="300ms">
                                 <h2 class="page-header">Log de <strong>Testes</strong></h2>
                                 <pre><code id="log" data-language="shell"></code></pre>
                             </div>
@@ -142,5 +227,12 @@
     <script type="text/javascript" src="js/bootbox.js"></script>
     <script type="text/javascript" src="js/ion.rangeSlider.min.js"></script>
     <script type="text/javascript" src="js/algoritmos-geneticos.js"></script>
+    <script type="text/javascript" src="js/rainbow/rainbow.min.js"></script>
+    <script type="text/javascript" src="js/rainbow/language/css.js"></script>
+    <script type="text/javascript" src="js/rainbow/language/generic.js"></script>
+    <script type="text/javascript" src="js/rainbow/language/html.js"></script>
+    <script type="text/javascript" src="js/rainbow/language/javascript.js"></script>
+    <script type="text/javascript" src="js/rainbow/language/php.js"></script>
+    <script type="text/javascript" src="js/rainbow/language/shell.js"></script>
 </body>
 </html>
